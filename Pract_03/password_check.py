2f4d49fe9b76693d9001ab5192099ec66ea01d31
Mary <marylouisegrace.couhualt@my.jcu.edu.au>
2021-07-30 10:43:43 +0800

    Changed Function Names

--- Pract_03/password_check.py
+++ Pract_03/password_check.py
@@ -8,11 +8,11 @@ Description: Asks user for a password and prints the equivalent length of *
 def main():
     password = input("What is your password?")
     pass_length = len(password)  # takes the password and gets its length
-    length(pass_length)
+    print_asterisks(pass_length)
 
 
-def length(pass_length):
-    for i in range(pass_length):
+def print_asterisks(pass_length):
+    for i in range(1, pass_length):
         print("*", end="")
 
 
