5b39a42676a6b0c4ca1f677af584a5a3525a6476
Mary <marylouisegrace.couhualt@my.jcu.edu.au>
2021-07-30 11:39:34 +0800

    Fixed the methods to randomly call a score

--- Pract_03/broken_score.py
+++ Pract_03/broken_score.py
@@ -10,7 +10,9 @@ def main():
     score = float(input("Enter score: "))
     get_score_response(score)
     print(get_score_response(score))
-    print(random.randint(get_score_response(score)))
+    random_score = random.randint(0, 100)
+    print("Your random score is:  {:.2f}".format(random_score))
+    print(get_score_response(random_score))
 
 
 def get_score_response(score):
