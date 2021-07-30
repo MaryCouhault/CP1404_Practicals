79dac9cae2051c0a3829b3c91a681b3b3380cc51
Mary <marylouisegrace.couhualt@my.jcu.edu.au>
2021-07-30 10:23:47 +0800

    Rewrote password checker to print * & added functions

--- Pract_03/password_check.py
+++ Pract_03/password_check.py
@@ -1,60 +1,19 @@
+"""CP1404
+Name: Mary Couhault
+Date: July 30th 2021
+Description: Asks user for a password and prints the equivalent length of *
 """
-CP1404/CP5632 - Practical
-Password checker "skeleton" code to help you get started
-"""
-
-MIN_LENGTH = 2
-MAX_LENGTH = 6
-SPECIAL_CHARS_REQUIRED = False
-SPECIAL_CHARACTERS = "!@#$%^&*()_-=+`~,./'[]<>?{}|\\"
 
 
 def main():
-    """Program to get and check a user's password."""
-    print("Please enter a valid password")
-    print("Your password must be between", MIN_LENGTH, "and", MAX_LENGTH,
-          "characters, and contain:")
-    print("\t1 or more uppercase characters")
-    print("\t1 or more lowercase characters")
-    print("\t1 or more numbers")
-    if SPECIAL_CHARS_REQUIRED:
-        print("\tand 1 or more special characters: ", SPECIAL_CHARACTERS)
-    password = input("> ")
-    while not is_valid_password(password):
-        print("Invalid password!")
-        password = input("> ")
-    print("Your {}-character password is valid: {}".format(len(password),
-                                                           password))
+    password = input("What is your password?")
+    pass_length = len(password)  # takes the password and gets its length
+    length(pass_length)
 
 
-def is_valid_password(password):
-    """Determine if the provided password is valid."""
-    # TODO: if length is wrong, return False
-    if len(password) < MIN_LENGTH or len(password) > MAX_LENGTH:
-        return False
-    count_lower = 0
-    count_upper = 0
-    count_digit = 0
-    count_special = 0
-    for char in password:
-        if char.isdigit():
-            count_digit += 1
-        elif char.islower():
-            count_lower += 1
-        elif char.isupper():
-            count_upper += 1
-        else:
-            count_special += 1
-
-        pass
-    if count_upper == 0 or count_lower == 0 or count_digit == 0:
-        return False
-    if SPECIAL_CHARS_REQUIRED:
-        if count_special == 0:
-            return False
-
-    # if we get here (without returning False), then the password must be valid
-    return True
+def length(pass_length):
+    for i in range(pass_length):
+        print("*", end="")
 
 
 main()
