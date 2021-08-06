9093ce4df043f12f41b1ca90bf231d39a49aec66
Mary <marylouisegrace.couhualt@my.jcu.edu.au>
2021-08-06 10:08:27 +0800

    Modified Code

---
+++ Pract_04/list_exercises.py
@@ -0,0 +1,13 @@
+from _ast import For
+
+five_number = []
+
+num_count = 0
+
+def main():
+    new_number_number = input("Write a Number: ")
+    for new_number_number in (1,6):
+        five_number.append(new_number_number)
+
+
+main()
