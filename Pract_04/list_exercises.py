e18aa8008f9ebd89c4725c8be2c242188a661d15
Mary <marylouisegrace.couhualt@my.jcu.edu.au>
2021-08-06 10:39:12 +0800

    Added Username Code

--- Pract_04/list_exercises.py
+++ Pract_04/list_exercises.py
@@ -1,4 +1,8 @@
+usernames = ['jimbo', 'giltson98', 'derekf', 'WhatSup', 'NicolEye', 'swei45', 'BaseInterpreterInterface', 'BaseStdIn', 'Command', 'ExecState', 'InteractiveConsole', 'InterpreterInterface', 'StartServer', 'bob']
+
+
 def main():
+
     numbers = []
     for i in range(1, 6):
         new_numbers = int(input(f"Write Number {i}: "))
@@ -10,5 +14,10 @@ def main():
     print("The largest number is", max(numbers))
     print("The average of the numbers is", sum(numbers) / len(numbers))
 
+    username = input("What is your username: ")
+    if username in usernames:
+        print("access granted")
+    else:
+        print("DENIED")
 
 main()
