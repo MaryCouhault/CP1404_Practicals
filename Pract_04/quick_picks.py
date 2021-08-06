4ab0528509677448cbdc4b5ed810f007ecdbc174
Mary <marylouisegrace.couhualt@my.jcu.edu.au>
2021-08-06 11:19:05 +0800

    First commit

--- Pract_04/quick_picks.py
+++ Pract_04/quick_picks.py
@@ -1,6 +1,7 @@
 import random
 
 number_gen = int(input("How many quick picks would you like to generate? "))
-for i in range (1, number_gen):
-    for n in range (1,6):
-        print(random.randint(1, 45))
+for i in range(0, number_gen):
+
+    for n in range(6):
+        print(random.randint(1, 45), end=" ")
