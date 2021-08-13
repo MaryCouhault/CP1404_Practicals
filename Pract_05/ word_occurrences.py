98a5aa28f8b8de740dbee28bbe062e2bad1a53e4
Mary <marylouisegrace.couhualt@my.jcu.edu.au>
2021-08-13 11:10:35 +0800

    Initial Commit

---
+++ Pract_05/ word_occurrences.py
@@ -0,0 +1,13 @@
+"""
+CP1404/CP5632 Practical
+User Enters email
+Program Prints their name
+"""
+
+
+def main():
+    email_capture = []
+    email = input("What is your email? ")
+    email_capture.append(email)
+    print(email_capture)
+main()
