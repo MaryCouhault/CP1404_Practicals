4f4a94b6511eebf5641d0b6ca68801a839766ac8
Mary <marylouisegrace.couhualt@my.jcu.edu.au>
2021-08-17 21:45:40 +0800

    Add in code from thonny

---
+++ Pract_05/emaill.py
@@ -0,0 +1,24 @@
+
+def main():
+    email_to_name = {}
+    email = input("Email: ")
+    while email != "":
+        name = name_from_email(email)
+        your_name = input("Is your name {}? (Y/n) ".format(name))
+        if your_name.upper() != "Y" and your_name != "":
+            name = input("Name: ")
+        email_to_name[email] = name
+        email = input("Email: ")
+
+    for email, name in email_to_name.items():
+        print("{} ({})".format(name, email))
+
+
+def name_from_email(email):
+    prefix = email.split('@')[0]
+    parts = prefix.split('.')
+    name = " ".join(parts).title()
+    return name
+
+
+main()
