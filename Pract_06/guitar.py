a7e2fa20e2fa67b1fa64838cc32bd69b47de55e2
Mary <marylouisegrace.couhualt@my.jcu.edu.au>
2021-08-31 00:57:43 +0800

    add base code from pract git

---
+++ Pract_06/guitar.py
@@ -0,0 +1,19 @@
+class Guitar:
+
+    def __init__(self, name, year, cost) -> None:
+        self.name = name
+        self.year = year
+        self.cost = cost
+
+    def __str__(self) -> str:
+        return f"{self.name}, ({self.year}): ${self.cost:.2f}"
+
+    def get_age(self):
+        year = 2021
+        return year - self.year
+
+    def is_vintage(self):
+        if self.get_age() >= 50:
+            return True
+        else:
+            return False
